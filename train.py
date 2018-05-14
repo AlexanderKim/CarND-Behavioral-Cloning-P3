bd34fd0cac31a8da9fd104e4ef3f9c72a4390db2
Alkim <alkim.mailbox@gmail.com>
2018-05-14 20:41:08 +0400

    3 epochs

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ def train_model(X_train, y_train, output_path):
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
     model.save(output_path)
 
