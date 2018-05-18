f95dce53915eff23f7e5862b2148064a6b2bf863
Alkim <alkim.mailbox@gmail.com>
2018-05-18 10:41:20 +0400

    Attempt to train with generator

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
+import numpy as np
+import cv2
+import os
+import sklearn
+
+
 def load_data(dir):
     import csv
-    import os
-    import cv2
-    import numpy as np
-    from pprint import pprint
 
     csv_path = os.path.join(dir, 'driving_log.csv')
-    img_path = os.path.join(dir, 'IMG')
+    # img_path = os.path.join(dir, 'IMG')
 
     with open(csv_path) as csvfile:
         images_steering = [
@@ -16,26 +18,51 @@ def load_data(dir):
             if line[0] != 'center'
         ]
 
-    X_train = [
-        cv2.imread(os.path.join(img_path, line[0]))
-        for line in images_steering
-    ]
+    return images_steering
 
-    y_train = [line[1] for line in images_steering]
+    # X_train = [
+    #     cv2.imread(os.path.join(img_path, line[0]))
+    #     for line in images_steering
+    # ]
+    #
+    # y_train = [line[1] for line in images_steering]
+    #
+    # X_train_flipped = [cv2.flip(img, 1) for img in X_train]
+    # y_train_reversed = y_train * -1
+    #
+    # for X, y in zip(X_train_flipped, y_train_reversed):
+    #     X_train.append(X)
+    #     y_train.append(y)
+    #
+    # X_train = np.array(X_train)
+    # y_train = np.array(y_train)
+    #
+    # return X_train, y_train
 
-    X_train_flipped = [cv2.flip(img, 1) for img in X_train]
-    y_train_reversed = y_train * -1
+def generator(samples, dir, batch_size=32):
+    img_path = os.path.join(dir, 'IMG')
 
-    for X, y in zip(X_train_flipped, y_train_reversed):
-        X_train.append(X)
-        y_train.append(y)
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = os.path.join(img_path, batch_sample[0])
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[1])
+                images.append(center_image)
+                angles.append(center_angle)
 
-    return X_train, y_train
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
 
-def train_model(X_train, y_train, output_path):
+def compile_model():
     from keras.models import Sequential
     from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, MaxPooling2D
 
@@ -57,9 +84,9 @@ def train_model(X_train, y_train, output_path):
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
-    model.save(output_path)
+    return model
+
 
 import argparse
 if __name__ == '__main__':
@@ -67,6 +94,22 @@ if __name__ == '__main__':
     argumets_parser.add_argument('--dir', type=str, default='.')
     args = argumets_parser.parse_args()
 
-    X_train, y_train = load_data(args.dir)
+    model = compile_model()
 
-    train_model(X_train, y_train, 'model.h5')
+    # X_train, y_train = load_data(args.dir)
+    # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+
+    from sklearn.model_selection import train_test_split
+    train_samples, validation_samples = train_test_split(load_data(args.dir), test_size=0.2)
+
+    train_generator = generator(train_samples, args.dir, batch_size=32)
+    validation_generator = generator(validation_samples, args.dir, batch_size=32)
+
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=len(train_samples),
+        validation_data = validation_generator,
+        nb_val_samples = len(validation_samples), nb_epoch = 3
+    )
+
+    model.save('model.h5')
